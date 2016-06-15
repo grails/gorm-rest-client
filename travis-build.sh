1c38fb8d6399d4ed4c10744bb6936bf7ec31aa94
graemerocher <graeme.rocher@gmail.com>
2016-06-15 15:26:59 +0200

    No plugins yet

--- travis-build.sh
+++ travis-build.sh
@@ -4,13 +4,14 @@ EXIT_STATUS=0
 ./gradlew --stop
 
 
-./gradlew test -no-daemon -x grails2-plugin:test -x gorm-neo4j-spring-boot:test  || EXIT_STATUS=$?
-if [[ $EXIT_STATUS -eq 0 ]]; then
-    ./gradlew grails2-plugin:test -no-daemon || EXIT_STATUS=$?
-fi
-if [[ $EXIT_STATUS -eq 0 ]]; then 
-    ./gradlew gorm-neo4j-spring-boot:test -no-daemon || EXIT_STATUS=$?
-fi
+./gradlew test -no-daemon
+#./gradlew test -no-daemon -x grails2-plugin:test -x gorm-neo4j-spring-boot:test  || EXIT_STATUS=$?
+#if [[ $EXIT_STATUS -eq 0 ]]; then
+#    ./gradlew grails2-plugin:test -no-daemon || EXIT_STATUS=$?
+#fi
+#if [[ $EXIT_STATUS -eq 0 ]]; then
+#    ./gradlew gorm-neo4j-spring-boot:test -no-daemon || EXIT_STATUS=$?
+#fi
 
 ./gradlew --stop
 
