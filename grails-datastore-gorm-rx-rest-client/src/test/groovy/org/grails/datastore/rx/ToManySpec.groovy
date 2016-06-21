1d78b7728d56b2ca9b20e87bf439338fc11041a4
Graeme Rocher <graeme.rocher@gmail.com>
2016-06-21 23:29:38 +0200

    test for reading ToMany association from embedded JSON

--- grails-datastore-gorm-rx-rest-client/src/test/groovy/org/grails/datastore/rx/ToManySpec.groovy
+++ grails-datastore-gorm-rx-rest-client/src/test/groovy/org/grails/datastore/rx/ToManySpec.groovy
@@ -135,4 +135,34 @@ class ToManySpec extends RxGormSpec {
         c.teams*.name.sort() == ["First Team", "Under 21s"]
         mock.verify()
     }
+
+
+    void "Test a hasMany association read from regular JSON embedded is already initialized"() {
+        given:"A canned response"
+        def mock = client.expect {
+            uri '/club/1'
+            accept(MediaType.HAL_JSON)
+        }
+        .respond {
+            json {
+                id 1
+                name "Manchester United"
+                teams( [
+                        [id:1L, name:"First Team"],
+                        [id:2L, name:"Under 21s"]
+                ])
+            }
+        }
+
+        when:"A get request is issued"
+        Observable<Club> observable = Club.get(1)
+        Club c = observable.toBlocking().first()
+
+        then:"The teams are a lazy collection"
+        c.teams instanceof Collection
+        c.teams.size() == 2
+        c.teams*.club.sort() == [c, c]
+        c.teams*.name.sort() == ["First Team", "Under 21s"]
+        mock.verify()
+    }
 }
