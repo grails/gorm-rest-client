8fb3b9f0629848e31cc19da84d485510d9c93f4b
graemerocher <graeme.rocher@gmail.com>
2016-06-23 09:11:53 +0200

    Add  initial documentation and Travis build for docs

--- travis-publish.sh
+++ travis-publish.sh
@@ -4,7 +4,7 @@ echo "Publishing..."
 
 EXIT_STATUS=0
 
-if [[ $TRAVIS_REPO_SLUG == "grails/gorm-neo4j" && $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0 ]]; then
+if [[ $TRAVIS_REPO_SLUG == "grails/gorm-rest-client" && $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0 ]]; then
 
   echo "Publishing archives"
   export GRADLE_OPTS="-Xmx1500m -Dfile.encoding=UTF-8"
@@ -27,9 +27,60 @@ if [[ $TRAVIS_REPO_SLUG == "grails/gorm-neo4j" && $TRAVIS_PULL_REQUEST == 'false
     # for snapshots only to repo.grails.org
     ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" publish || EXIT_STATUS=$?
   fi
+
+
   if [[ $EXIT_STATUS -eq 0 ]]; then
     echo "Publishing Successful."
+
+    echo "Publishing Documentation..."
+    ./gradlew docs:docs
+
+    git config --global user.name "$GIT_NAME"
+    git config --global user.email "$GIT_EMAIL"
+    git config --global credential.helper "store --file=~/.git-credentials"
+    echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
+
+    git clone https://${GH_TOKEN}@github.com/grails/grails-data-mapping.git -b gh-pages gh-pages --single-branch > /dev/null
+    cd gh-pages
+    mkdir -p rx/rest-client
+    cd rx/rest-client
+
+    if [[ -n $TRAVIS_TAG ]]; then
+        version="$TRAVIS_TAG"
+        version=${version:1}
+
+         mkdir -p latest
+         cp -r ../../../docs/build/docs/. ./latest/
+         git add latest/*
+
+        majorVersion=${version:0:4}
+        majorVersion="${majorVersion}x"
+
+        mkdir -p "$version"
+        cp -r ../../../docs/build/docs/. "./$version/"
+        git add "$version/*"
+
+        mkdir -p "$majorVersion"
+        cp -r ../../../docs/build/docs/. "./$majorVersion/"
+        git add "$majorVersion/*"
+
+    else
+        # If this is the master branch then update the snapshot
+        mkdir -p snapshot
+        cp -r ../../../docs/build/docs/. ./snapshot/
+
+        git add snapshot/*
+    fi
+
+
+    git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+    git push origin HEAD
+    cd ../../..
+    rm -rf gh-pages
   fi
+
+
   
 fi
 
