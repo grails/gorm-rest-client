8f97ab3a8dce4cf876ae5e23f10b19ff5d181568
Graeme Rocher <graeme.rocher@gmail.com>
2016-07-20 09:19:48 +0200

    Debug Travis build

--- travis-build.sh
+++ travis-build.sh
@@ -4,14 +4,7 @@ EXIT_STATUS=0
 ./gradlew --stop
 
 
-./gradlew check --refresh-dependencies -no-daemon || EXIT_STATUS=$?
-#./gradlew test -no-daemon -x grails2-plugin:test -x gorm-neo4j-spring-boot:test  || EXIT_STATUS=$?
-#if [[ $EXIT_STATUS -eq 0 ]]; then
-#    ./gradlew grails2-plugin:test -no-daemon || EXIT_STATUS=$?
-#fi
-#if [[ $EXIT_STATUS -eq 0 ]]; then
-#    ./gradlew gorm-neo4j-spring-boot:test -no-daemon || EXIT_STATUS=$?
-#fi
+./gradlew check --refresh-dependencies -no-daemon -i || EXIT_STATUS=$?
 
 ./gradlew --stop
 
